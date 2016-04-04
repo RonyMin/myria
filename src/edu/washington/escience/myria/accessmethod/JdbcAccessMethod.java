9dd8260a04f12d88d29ec6c8181629604ea51de2
Parmita <parmitam@hotmail.com>
2016-04-04 11:54:47 -0700

    postgres

--- src/edu/washington/escience/myria/accessmethod/JdbcAccessMethod.java
+++ src/edu/washington/escience/myria/accessmethod/JdbcAccessMethod.java
@@ -409,7 +409,7 @@ public final class JdbcAccessMethod extends AccessMethod {
       case DATETIME_TYPE:
         return "TIMESTAMP";
       case BYTES_TYPE:
-        return "BLOB";
+        return "BYTEA";
       default:
         throw new UnsupportedOperationException("Type " + type + " is not supported by DBMS " + dbms);
     }
