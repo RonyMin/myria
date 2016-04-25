a8ea20d7dc6d20e0a53a63a0153fb78735c2f9fe
Parmita <parmitam@hotmail.com>
2016-04-25 11:53:50 -0700

    adding python changes

--- python/MyriaPythonUDF/code.py
+++ python/MyriaPythonUDF/code.py
@@ -2,12 +2,10 @@
 
 import socket               # Import socket module
 import sys
-import struct
-import cPickle
 import os
-import sys
-import cloud
-
+import numpy as np
+from dipy.denoise import nlmeans
+from dipy.denoise.noise_estimate import estimate_sigma
 from MyriaPythonUDF.serializers import read_int, write_int,SpecialLengths,write_with_length,CloudPickleSerializer
 
 # def sendMsg(sock_file,data):
@@ -23,34 +21,21 @@ if __name__ == '__main__':
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   #hostname = socket.gethostname()
   sock.connect(("127.0.0.1",java_port))
-  #sock.connect((hostname,12345))
-
-  #f = open("logfile.txt",'wb')
-
-  #infile = os.fdopen(os.dup(sock.fileno()), "rb", 65536)
   outfile = os.fdopen(os.dup(sock.fileno()), "wb", 65536)
 
   def foo(dt):
-      import  numpy as np
-      from dipy.denoise import nlmeans
-      from dipy.denoise.noise_estimate import estimate_sigma
-      #print "called!"
-      #while True:
-        #dt = x.next()
       image = dt[0]
       mask = dt[1]
       sigma = estimate_sigma(image)
       denoised_data = nlmeans.nlmeans(image, sigma=sigma, mask=mask)
-      #print "finished denoising"
       return denoised_data
-        #yield denoised_data
+
 
 
 
   ser = CloudPickleSerializer()
-  #x = ser.dumps(foo)
   ser.write_with_length(foo,outfile)
-  #x = cloud.serialization.cloudpickle.dumps(foo)
+
 
 
   #sendMsg(outfile, x)
--- python/MyriaPythonUDF/worker.py
+++ python/MyriaPythonUDF/worker.py
@@ -21,26 +21,15 @@ def main(infile, outfile):
     try:
         #get code
         func = read_command(infile, pickleSer)
-        #print (type(func))
-
-        #first thing an iterator writes is the number of items in a tuple
-        #to be passed to the function
         tuplesize = read_int(infile)
+
         if tuplesize < 1:
             raise ValueError("size of tuple should not be less than 1 ")
 
-        i=1
+
         while True:
-            #iterator = pickleSer.load_stream(infile, tuplesize)
-            #pickleSer.dump_stream(func(iterator),outfile)
-            #print ("tuple number"+str(i), file=sys.stdout)
             tup =pickleSer.read_with_length(infile,tuplesize)
-            #print ("Read tuple", file=sys.stdout)
-            result = func(tup)
-            #print ("got results back", file=sys.stdout)
-            pickleSer.write_with_length(result,outfile)
-            #print ("wrote results back", file=sys.stdout)
-            i = i+1
+            pickleSer.write_with_length(func(tup),outfile)
             outfile.flush()
 
 
@@ -53,19 +42,19 @@ def main(infile, outfile):
             print("python process failed with exception: ", file=sys.stderr)
             print(traceback.format_exc(), file=sys.stderr)
 
-    if read_int(infile) == SpecialLengths.END_OF_STREAM:
-        write_int(SpecialLenghts.END_OF_STREAM, outfile)
-    else:
-        write_int(SpecialLengths.END_OF_DATA_SECTION,outfile)
-        exit(-1)
+    #if read_int(infile) == SpecialLengths.END_OF_STREAM:
+    #    write_int(SpecialLenghts.END_OF_STREAM, outfile)
+    #else:
+    #    write_int(SpecialLengths.END_OF_DATA_SECTION,outfile)
+    #    exit(-1)
 
 if __name__ == '__main__':
     # Read a local port to connect to from stdin
     java_port = int(sys.stdin.readline())
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("127.0.0.1", java_port))
-    infile = os.fdopen(os.dup(sock.fileno()), "a+", 262144)
-    outfile = os.fdopen(os.dup(sock.fileno()), "a+", 262144)
+    infile = os.fdopen(os.dup(sock.fileno()), "rb", 65536)
+    outfile = os.fdopen(os.dup(sock.fileno()), "wb", 65536)
     exit_code = 0
     try:
         main(infile,outfile)
